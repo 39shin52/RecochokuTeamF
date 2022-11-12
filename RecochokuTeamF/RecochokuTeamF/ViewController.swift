68fc547e134e9b7f388405092b61df0af4979632
Shingo Itoh <g2122006@fun.ac.jp>
2022-11-12 18:02:22 +0900

    inital commit

---
+++ RecochokuTeamF/RecochokuTeamF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecochokuTeamF
+//
+//  Created by Itou Shingo on 2022/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
